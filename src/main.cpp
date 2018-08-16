54b066f0f0fc9f346842e7702dc01b69d5b6d2d1
vzwGrey <vzwgrey@gmail.com>
2018-08-16 17:26:55 +0200

    Add filter functions to bottom

--- src/main.cpp
+++ src/main.cpp
@@ -4,21 +4,8 @@
 
 using namespace cv;
 
-void filter_greyscale(Mat &image) {
-	for (int y = 0; y < image.rows; y++) {
-		for (int x = 0; x < image.cols; x++ ) {
-			Vec3b &pixel = image.at<Vec3b>(x, y);
-			float avg = (pixel[0] + pixel[1] + pixel[2]) / 3;
-			for (int i = 0; i < 3; i++) pixel[i] = avg;
-		}
-	}
-}
-
-void filter_rotate(Mat &image, float angle) {
-	Point2f center(image.rows / 2, image.cols / 2);
-	Mat rotation = getRotationMatrix2D(center, angle, 1.0);
-	warpAffine(image, image, rotation, image.size());
-}
+void filter_greyscale(Mat &image);
+void filter_rotate(Mat &image, float angle);
 
 int main(int argc, char **argv) {
 	if (argc < 2) {
@@ -54,5 +41,21 @@ int main(int argc, char **argv) {
 	imwrite("output.png", image);
 
 	return 0;
+}
+
+void filter_rotate(Mat &image, float angle) {
+	Point2f center(image.rows / 2, image.cols / 2);
+	Mat rotation = getRotationMatrix2D(center, angle, 1.0);
+	warpAffine(image, image, rotation, image.size());
+}
+
+void filter_greyscale(Mat &image) {
+	for (int y = 0; y < image.rows; y++) {
+		for (int x = 0; x < image.cols; x++ ) {
+			Vec3b &pixel = image.at<Vec3b>(x, y);
+			float avg = (pixel[0] + pixel[1] + pixel[2]) / 3;
+			for (int i = 0; i < 3; i++) pixel[i] = avg;
+		}
+	}
 }
 
